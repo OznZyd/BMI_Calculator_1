e93facd325645bd135191f9b82f47ce188948a67
Ozan Zeydanoglu <ozanzeydanoglu@gmail.com>
2025-11-08 12:58:54 +0100

    Implement range mapping for accurate needle angle and handle out-of-range BMI values

--- main.py
+++ main.py
@@ -17,22 +17,39 @@ def calculate_bmi():
         weight_num = float(weight_str)
         height_m = height_num / 100
         bmi_resulth = weight_num / height_m ** 2
-        bmi_angle = 6 * (bmi_resulth-10)
-        x2 = 150 + (120 * math.cos(math.radians(bmi_angle)))
-        y2 = 140 - (120 * math.sin(math.radians(bmi_angle)))
-        line = canvas.create_line(150, 140, x2, y2, width=3, fill="black", tags="needle", arrow= "last")
+
+        bmi_for_angle = bmi_resulth
+
+        if bmi_for_angle > 40:
+            bmi_for_angle = 40
+        elif bmi_for_angle < 10:
+            bmi_for_angle = 10
 
         if bmi_resulth < 14.0:
+            persentage = (bmi_for_angle - 10) / (14 - 10)
+            bmi_angle = 0 + (persentage * (36 - 0))
             category_str = "You are Very Weak person 😮"
         elif bmi_resulth < 18.8:
+            persentage = (bmi_for_angle - 14) / (18.8 - 14)
+            bmi_angle = 36 + (persentage * (72 - 36))
             category_str = "You are Weak person 😟"
         elif bmi_resulth < 24.9:
+            persentage = (bmi_for_angle - 18.8) / (24.9 - 18.8)
+            bmi_angle = 72 + (persentage * (108 - 72))
             category_str = "You are Normal person! 😊"
         elif bmi_resulth < 29.9:
+            persentage = (bmi_for_angle - 24.9) / (29.9 - 24.9)
+            bmi_angle =  108 + (persentage *(144-108))
             category_str = "You are Overweight person! 😐"
         else:
+            persentage = (bmi_for_angle - 29.9) / (40 - 29.9)
+            bmi_angle = 144 + (persentage * (180 - 144))
             category_str = "You are Obese Person! 😮"
 
+        x2 = 150 + (120 * math.cos(math.radians(bmi_angle)))
+        y2 = 140 - (120 * math.sin(math.radians(bmi_angle)))
+
+        line = canvas.create_line(150, 140, x2, y2, width=3, fill="black", tags="needle", arrow="last")
         resulth_label.config(text= f"Your BMI score : {bmi_resulth:.1f}, BMI Catagories : {category_str}")
 
     except ValueError:
